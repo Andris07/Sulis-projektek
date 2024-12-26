a3bd1a9041918501c77f694ddf65e76ef78b9190
Andris <laczkovicsandris07.education@gmail.com>
2024-12-26 19:41:01 +0100

    Add files via upload

---
+++ 2024-2025/Szakmai tantárgyak/Adatbázis - kezelés/Gyakorlat/10.04_órai/laczkovics-andris-napfogyatkozas.sql
@@ -0,0 +1,24 @@
+-- 3. feladat
+SELECT * FROM `napfogyatkozas` ORDER BY `datum` ASC;
+-- 4. feladat
+SELECT DISTINCT `tipus` FROM `napfogyatkozas`;
+-- 5. feladat
+SELECT * FROM `napfogyatkozas` WHERE `szelesseg` IS NULL;
+-- 6. feladat
+SELECT `datum`, `ido`, `tipus` FROM `napfogyatkozas` WHERE `megfigyelheto`='Észak-Amerika';
+-- 7. feladat
+SELECT `datum`, `ido`, `megfigyelheto` FROM `napfogyatkozas` WHERE (`tipus`='teljes' OR `tipus`='gyűrűs');
+-- 8. feladat
+SELECT `datum`, `ido` FROM `napfogyatkozas` WHERE `megfigyelheto` LIKE '%Afrika%' AND CURRENT_DATE() < `datum` AND(`tipus`='teljes' OR `tipus`='gyűrűs');
+-- 9. feladat
+SELECT `datum`, `ido`, `megfigyelheto` FROM `napfogyatkozas` WHERE `megfigyelheto` IS NOT NULL;
+-- 10. feladat
+SELECT `datum`, `ido` FROM `napfogyatkozas` WHERE `tipus`='teljes' AND `megfigyelheto` LIKE '%magyarorszag%';
+-- 11. feladat
+SELECT DISTINCT `tipus` FROM `napfogyatkozas` ORDER BY `tipus` ASC;
+-- 12. feladat
+SELECT `datum`, `ido`, `idotartam` FROM `napfogyatkozas` WHERE `tipus`='teljes' ORDER BY `idotartam` DESC;
+-- 13. feladat
+SELECT * FROM `napfogyatkozas` WHERE `megfigyelheto` LIKE '%,%';
+-- 14. feladat
+SELECT * FROM `napfogyatkozas` WHERE `tipus`='részleges' AND (`megfigyelheto` LIKE '%alaszka' OR `megfigyelheto` LIKE '%hawaii');
