797e7a101e5a09bfd5f80b604d039728a21d781e
Andris <laczkovicsandris07.education@gmail.com>
2025-01-10 14:08:47 +0100

    set sql mode

--- 2024-2025/Szakmai tantárgyak/Adatbázis - kezelés/Gyakorlat/09.27_órai/laczkovics-andris-autoskartya1.sql
+++ 2024-2025/Szakmai tantárgyak/Adatbázis - kezelés/Gyakorlat/09.27_órai/laczkovics-andris-autoskartya1.sql
@@ -1,7 +1,8 @@
+SET sql_mode = 'ANSI,ONLY_FULL_GROUP_BY';
 -- 3. feladat
 SELECT * FROM `autoskartya`;
 -- 4. feladat
-SELECT `gyartmany`, `tipus` FROM `autoskartya` WHERE `orszag`='Németország' AND(`vegsebesseg` > 240 OR `loero` >= 200) ORDER BY `tipus` DESC;
+SELECT `gyartmany`, `tipus` FROM `autoskartya` WHERE `orszag`='Németország' AND (`vegsebesseg` > 240 OR `loero` >= 200) ORDER BY `tipus` DESC;
 -- 5. feladat
 SELECT * FROM `autoskartya` WHERE (`orszag`='Japán' OR `fogyasztas` <= 6 OR `hengerurtartalom` <= 1400 AND `fogyasztas` <= 7);
 -- 6. feladat
@@ -13,4 +14,4 @@ SELECT `gyartmany`, `tipus`, `loero` FROM `autoskartya` ORDER BY `loero` DESC;
 -- 9. feladat
 SELECT `tipus`, `orszag`, `vegsebesseg` FROM `autoskartya` ORDER BY `orszag` ASC;
 -- 10. feladat
-SELECT `gyartmany`, `tipus`, `orszagkod` FROM `autoskartya` WHERE `fogyasztas`<5 AND `orszag` IN('Németország', 'Franciaország');
+SELECT `gyartmany`, `tipus`, `orszagkod` FROM `autoskartya` WHERE `fogyasztas` < 5 AND `orszag` IN('Németország', 'Franciaország');
